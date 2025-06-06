eff0bd7ee07d9c97ee93064a16905fd7847893f0
burdzaki <burdzaki@gmail.com>
2025-06-06 20:51:48 -0300

    feat: atualiza testes e arquivo main

--- src/__tests__/projeto-disp.test.ts
+++ src/__tests__/projeto-disp.test.ts
@@ -1,29 +1,77 @@
 import { VortexParameters } from "../calculation";
 
 describe("VortexParameters", () => {
-    test("calcula esbeltez corretamente", () => {
+    // test("Cálculo de Esbeltez", () => {
+    //     const vp = new VortexParameters(
+    //     80,             // structureHeight
+    //     11,              // dimensionD0
+    //     45,             // speedV0
+    //     1.0,            // topographicFactorS1
+    //     1.0,            // statisticalFactorS3
+    //     60,             // elevationZ
+    //     "Category IV",  // structureCategory
+    //     0.12,           // structureFrequencyFn
+    //     13,              // transversalDimensionL
+    //     "Rectangle",       // structureForm
+    //     "Horizontal",   // windDirection
+    //     13,              // widthA
+    //     22               // lenghtB
+    //     );  
+        
+    //     const result = vp.calculateSlenderness();
+
+    //     console.log("Esbeltez =", vp.slenderness);
+    //     expect(result).toContain("devem ser investigados");
+    // });
+
+    // test("Cálculo do fator S2", () => {
+    //     const vp = new VortexParameters(
+    //     2000,             // structureHeight
+    //     13,              // dimensionD0
+    //     90,             // speedV0
+    //     1.0,            // topographicFactorS1
+    //     1.0,            // statisticalFactorS3
+    //     80,             // elevationZ
+    //     "Category VI",  // structureCategory
+    //     0.12,           // structureFrequencyFn
+    //     13,              // transversalDimensionL
+    //     "Rectangle",       // structureForm
+    //     "Horizontal",   // windDirection
+    //     13,              // widthA
+    //     22               // lenghtB        
+    //     );
+    
+    //     const result = vp.calculateFactorS2();
+    //     console.log("bm =", vp.meteorologicalParameterBm);
+    //     console.log("p =", vp.exponentP);
+    //     console.log("S2 =", vp.roughnessFactorS2);
+
+    //     expect(vp.meteorologicalParameterBm["Category IV"]).toBe(0.71);
+    //     expect(vp.exponentP["Category IV"]).toBe(0.23);
+    //     expect(result).toBeCloseTo(1.07, 2);
+    // });
+
+    test("Cálculo da Vest", () => {
         const vp = new VortexParameters(
-        12, // height
-        2,  // D0
-        30, 1.0, 1.0, 15, "Category II", 0.69, 2, "Circle", "Horizontal", 1, 2
+        80,             // structureHeight
+        13,              // dimensionD0
+        450000,             // speedV0
+        1.0,            // topographicFactorS1
+        1.0,            // statisticalFactorS3
+        80,             // elevationZ
+        "Category IV",  // structureCategory
+        0.12,           // structureFrequencyFn
+        13,              // transversalDimensionL
+        "Rectangle",       // structureForm
+        "Horizontal",   // windDirection
+        13,              // widthA
+        22               // lenghtB        
         );
+    
+        //vp.calculateFactorS2();
+        const result = vp.calculateStructureSpeed();
 
-        const result = vp.calculateSlenderness();
-        expect(result).toContain("devem ser investigados");
+        expect(result).toBeCloseTo(44.46, 2);
     });
 
-    test("calcula expoentP e parameterBM corretamente", () => {
-        const vpS2 = new VortexParameters (
-        60,
-        5,
-        30,
-        1.0,
-        1.0,
-        60,
-        "Category II", 0.69, 2, "Circle", "Horizontal", 1, 2
-        );
-
-        const resultS2 = vpS2.calculateFactorS2();
-        expect(resultS2).toBeCloseTo(0.90,2);
-    });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { getElement } from "./utils/dom";
 import { getParameter, validateParameter } from "./input";
-import { calculateParameters, calculateSlenderness } from "./calculation";
+//import { calculateParameters, calculateSlenderness } from "./calculation";
 import { showCalculusResult, showSlendernessResult } from "./output";
 
 const buttonCalculate = getElement<HTMLButtonElement>('.input__button');
 
-validateParameter('#h', '#warning-h');
-validateParameter('#d0', '#warning-d0');
+validateParameter('#structure-height', '#warning-structure-height');
+validateParameter('#dimension-d0', '#warning-dimension-d0');
 
-validateParameter('#V0', '#warning-V0');
-validateParameter('#S1', '#warning-S1');
+validateParameter('#speed-V0', '#warning-speed-V0');
+validateParameter('#factor-S1', '#warning-factor-S1');
 
 
-buttonCalculate.addEventListener('click', () => {
-    let h : number = getParameter('#h');
-    let d0 : number = getParameter('#d0');
-    let slendernessRatio : number = calculateSlenderness(h, d0);
-    showSlendernessResult(slendernessRatio);
+// buttonCalculate.addEventListener('click', () => {
+//     let h : number = getParameter('#structure-height');
+//     let d0 : number = getParameter('#dimension-d0');
+//     let slendernessRatio : number = calculateSlenderness(h, d0);
+//     showSlendernessResult(slendernessRatio);
 
-    let V0 : number = getParameter('#V0');
-    let S1 : number = getParameter('#S1');
-    let resultNumber : number = calculateParameters(V0, S1);
-    showCalculusResult(resultNumber);
-});
+//     let V0 : number = getParameter('#speed-V0');
+//     let S1 : number = getParameter('#factor-S1');
+//     let resultNumber : number = calculateParameters(V0, S1);
+//     showCalculusResult(resultNumber);
+// });
