869abde32258c7c2cbbc465479d976be4c2cd765
burdzaki <burdzaki@gmail.com>
2025-08-10 09:21:27 -0300

    feat: configura vite.config para montagem de tunel

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+
+    // ✅ Libere o domínio do Cloudflare Tunnel
+    // Se quiser permitir qualquer subdomínio do trycloudflare:
+    allowedHosts: ['expressions-cardiac-tough-fe.trycloudflare.com'],
+    // ou, para liberar só o host que apareceu no seu terminal:
+    // allowedHosts: ['renew-catherine-standings-did.trycloudflare.com'],
+
+    // (opcional) ajuda o HMR atrás de HTTPS
+    // hmr: { protocol: 'wss', port: 443 }
+  },
+})
